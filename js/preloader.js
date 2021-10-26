6e95f5fb0bd767e423b2c22ab2ca4a6c45bc1e83
YashwanthGathuku <yashwanth8035@gmail.com>
2021-10-26 18:10:46 -0400

    js

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+// function that gets rid of preload
+window.addEventListener("load", () => {
+    const preload = document.querySelector(".preload");
+    preload.classList.add("preload-finish")
+    
+ 
+});
